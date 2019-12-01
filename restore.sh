bd9748870e6443aeb3697711d8fcc1eca2f90f6c
Par <pacroy@gmail.com>
2019-12-01 09:47:27 +0700

    Fixed kc => kubectl

--- restore.sh
+++ restore.sh
@@ -14,5 +14,5 @@ kubectl exec -it --context $KUBE_CONTEXT --namespace $WIKI_NAMSPACE $MYSQL_POD_N
 BOOKSTACK_POD_NAME=$(kubectl get pod -o name -l app=bookstack --context $KUBE_CONTEXT --namespace $WIKI_NAMSPACE | head -1 | grep -o '[^/]*$')
 kubectl cp --context $KUBE_CONTEXT --namespace $WIKI_NAMSPACE ./backup/uploads.tgz $BOOKSTACK_POD_NAME:/root/uploads.tgz
 kubectl exec -it --context $KUBE_CONTEXT --namespace $WIKI_NAMSPACE $BOOKSTACK_POD_NAME -- bash -c "tar -xvzf /root/uploads.tgz -C /var/www/bookstack/public/uploads && rm /root/uploads.tgz && exit"
-kc scale --replicas=0 deploy/bookstack
-kc scale --replicas=1 deploy/bookstack
+kubectl scale --replicas=0 deploy/bookstack
+kubectl scale --replicas=1 deploy/bookstack
