ad49f4b55630b096e8b568064f3fe41cb549fe17
Par <pacroy@gmail.com>
2020-05-02 15:04:20 +0000

    Updated tgz location to /var instead of /root

--- backup.sh
+++ backup.sh
@@ -21,12 +21,12 @@ kubectl exec -it --context $KUBE_CONTEXT --namespace=$WIKI_NAMSPACE $MYSQL_POD_N
 BOOKSTACK_POD_NAME=$(kubectl get pod -o name -l app=$BOOKSTACK_APP_LABEL --context $KUBE_CONTEXT --namespace=$WIKI_NAMSPACE | head -1 | grep -o '[^/]*$')
 [ -z "$BOOKSTACK_POD_NAME" ] && echo "ERROR: Cannot find a $BOOKSTACK_APP_LABEL pod" && exit 1
 echo -e "\nArchiving BookStack Uploads from $BOOKSTACK_POD_NAME..."
-kubectl exec -it --context $KUBE_CONTEXT --namespace=$WIKI_NAMSPACE $BOOKSTACK_POD_NAME -- bash -c "rm -f ~/uploads.tgz && cd /var/www/bookstack/public/uploads/ && tar -cvzf ~/uploads.tgz * | wc -l | xargs -i echo {} 'file(s) archived' && exit"
+kubectl exec -it --context $KUBE_CONTEXT --namespace=$WIKI_NAMSPACE $BOOKSTACK_POD_NAME -- bash -c "rm -f /var/uploads.tgz && cd /var/www/bookstack/public/uploads/ && tar -cvzf /var/uploads.tgz * | wc -l | xargs -i echo {} 'file(s) archived' && exit"
 echo -e "\nCopying BookStack Uploads from $BOOKSTACK_POD_NAME..."
-kubectl cp --context $KUBE_CONTEXT --namespace=$WIKI_NAMSPACE $BOOKSTACK_POD_NAME:/root/uploads.tgz ./backup/uploads.tgz
-kubectl exec -it --context $KUBE_CONTEXT --namespace=$WIKI_NAMSPACE $BOOKSTACK_POD_NAME -- bash -c "rm -f ~/uploads.tgz"
+kubectl cp --context $KUBE_CONTEXT --namespace=$WIKI_NAMSPACE $BOOKSTACK_POD_NAME:/var/uploads.tgz ./backup/uploads.tgz
+kubectl exec -it --context $KUBE_CONTEXT --namespace=$WIKI_NAMSPACE $BOOKSTACK_POD_NAME -- bash -c "rm -f /var/uploads.tgz"
 echo -e "\nArchiving BookStack Storage from $BOOKSTACK_POD_NAME..."
-kubectl exec -it --context $KUBE_CONTEXT --namespace=$WIKI_NAMSPACE $BOOKSTACK_POD_NAME -- bash -c "rm -f ~/storage.tgz && cd /var/www/bookstack/storage/ && tar -cvzf ~/storage.tgz * | wc -l | xargs -i echo {} 'file(s) archived' && exit"
+kubectl exec -it --context $KUBE_CONTEXT --namespace=$WIKI_NAMSPACE $BOOKSTACK_POD_NAME -- bash -c "rm -f /var/storage.tgz && cd /var/www/bookstack/storage/ && tar -cvzf /var/storage.tgz * | wc -l | xargs -i echo {} 'file(s) archived' && exit"
 echo -e "\nCopying BookStack Storage from $BOOKSTACK_POD_NAME..."
-kubectl cp --context $KUBE_CONTEXT --namespace=$WIKI_NAMSPACE $BOOKSTACK_POD_NAME:/root/storage.tgz ./backup/storage.tgz
-kubectl exec -it --context $KUBE_CONTEXT --namespace=$WIKI_NAMSPACE $BOOKSTACK_POD_NAME -- bash -c "rm -f ~/storage.tgz"
+kubectl cp --context $KUBE_CONTEXT --namespace=$WIKI_NAMSPACE $BOOKSTACK_POD_NAME:/var/storage.tgz ./backup/storage.tgz
+kubectl exec -it --context $KUBE_CONTEXT --namespace=$WIKI_NAMSPACE $BOOKSTACK_POD_NAME -- bash -c "rm -f /var/storage.tgz"
