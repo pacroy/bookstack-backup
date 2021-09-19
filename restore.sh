242cde9f92c5a0d514ab92d8c3660b24b7139285
Par <pacroy@gmail.com>
2021-09-19 14:32:39 +0700

    Add container argument

--- restore.sh
+++ restore.sh
@@ -26,28 +26,28 @@ MYSQL_PODS=$(kubectl get pod -o name -l app=$MYSQL_APP_LABEL --context $KUBE_CON
 [ -z "$MYSQL_PODS" ] && echo "ERROR: Cannot find any $MYSQL_APP_LABEL pod" >&2 && exit 1
 MYSQL_POD_NAME=$(echo ${MYSQL_PODS} | head -1 | grep -o '[^/]*$')
 echo -e "\nCopying MySQL DB Backup into $MYSQL_POD_NAME..."
-kubectl cp --context $KUBE_CONTEXT --namespace=$WIKI_NAMSPACE ./backup/bookstack.sql $MYSQL_POD_NAME:/root/bookstack.sql
+kubectl cp --context $KUBE_CONTEXT --namespace=$WIKI_NAMSPACE ./backup/bookstack.sql -c bookstack-mysql $MYSQL_POD_NAME:/root/bookstack.sql
 if { [ -z "$HOST_FROM" ] || [ -z "$HOST_TO" ]; }; then 
     echo -e "\nHOST_FROM and/or HOST_TO not specified. Skip updating hostname."
 else
     echo -e "\nUpdating hostname from '$HOST_FROM' to '$HOST_TO'..."
-    kubectl exec -it --context $KUBE_CONTEXT --namespace=$WIKI_NAMSPACE $MYSQL_POD_NAME -- bash -c "sed -i'.bak' -e 's/$HOST_FROM/$HOST_TO/g' /root/bookstack.sql"
+    kubectl exec -it --context $KUBE_CONTEXT --namespace=$WIKI_NAMSPACE -c bookstack-mysql $MYSQL_POD_NAME -- bash -c "sed -i'.bak' -e 's/$HOST_FROM/$HOST_TO/g' /root/bookstack.sql"
 fi 
 echo -e "\nRestoring MySQL DB on $MYSQL_POD_NAME..."
-kubectl exec -it --context $KUBE_CONTEXT --namespace=$WIKI_NAMSPACE $MYSQL_POD_NAME -- bash -c "echo 'FLUSH PRIVILEGES;' >> /root/bookstack.sql && MYSQL_PWD=secret mysql < /root/bookstack.sql && rm /root/bookstack.sql && exit"
+kubectl exec -it --context $KUBE_CONTEXT --namespace=$WIKI_NAMSPACE -c bookstack-mysql $MYSQL_POD_NAME -- bash -c "echo 'FLUSH PRIVILEGES;' >> /root/bookstack.sql && MYSQL_PWD=secret mysql < /root/bookstack.sql && rm /root/bookstack.sql && exit"
 
 # Restore Bookstack
 BOOKSTACK_PODS=$(kubectl get pod -o name -l app=$BOOKSTACK_APP_LABEL --context $KUBE_CONTEXT --namespace=$WIKI_NAMSPACE)
 [ -z "$BOOKSTACK_PODS" ] && echo "ERROR: Cannot find any $BOOKSTACK_APP_LABEL pod" >&2 && exit 1
 BOOKSTACK_POD_NAME=$(echo ${BOOKSTACK_PODS} | head -1 | grep -o '[^/]*$')
 echo -e "\nCopying Bookstack Uploads Backup into $BOOKSTACK_POD_NAME..."
-kubectl cp --context $KUBE_CONTEXT --namespace=$WIKI_NAMSPACE ./backup/uploads.tgz $BOOKSTACK_POD_NAME:/var/www/bookstack/uploads.tgz
+kubectl cp --context $KUBE_CONTEXT --namespace=$WIKI_NAMSPACE ./backup/uploads.tgz -c bookstack $BOOKSTACK_POD_NAME:/var/www/bookstack/uploads.tgz
 echo -e "\nExtracting Bookstack Uploads Backup on $BOOKSTACK_POD_NAME..."
-kubectl exec -it --context $KUBE_CONTEXT --namespace=$WIKI_NAMSPACE $BOOKSTACK_POD_NAME -- bash -c "tar -xvzf /var/www/bookstack/uploads.tgz -C /var/www/bookstack/public/uploads | wc -l | xargs -i echo {} 'file(s) extracted' && rm /var/www/bookstack/uploads.tgz && exit"
+kubectl exec -it --context $KUBE_CONTEXT --namespace=$WIKI_NAMSPACE -c bookstack $BOOKSTACK_POD_NAME -- bash -c "tar -xvzf /var/www/bookstack/uploads.tgz -C /var/www/bookstack/public/uploads | wc -l | xargs -i echo {} 'file(s) extracted' && rm /var/www/bookstack/uploads.tgz && exit"
 echo -e "\nCopying Bookstack Storage Backup into $BOOKSTACK_POD_NAME..."
-kubectl cp --context $KUBE_CONTEXT --namespace=$WIKI_NAMSPACE ./backup/storage.tgz $BOOKSTACK_POD_NAME:/var/www/bookstack/storage.tgz
+kubectl cp --context $KUBE_CONTEXT --namespace=$WIKI_NAMSPACE ./backup/storage.tgz -c bookstack $BOOKSTACK_POD_NAME:/var/www/bookstack/storage.tgz
 echo -e "\nExtracting Bookstack Storage Backup on $BOOKSTACK_POD_NAME..."
-kubectl exec -it --context $KUBE_CONTEXT --namespace=$WIKI_NAMSPACE $BOOKSTACK_POD_NAME -- bash -c "tar -xvzf /var/www/bookstack/storage.tgz -C /var/www/bookstack/storage | wc -l | xargs -i echo {} 'file(s) extracted' && rm /var/www/bookstack/storage.tgz && exit"
+kubectl exec -it --context $KUBE_CONTEXT --namespace=$WIKI_NAMSPACE -c bookstack $BOOKSTACK_POD_NAME -- bash -c "tar -xvzf /var/www/bookstack/storage.tgz -C /var/www/bookstack/storage | wc -l | xargs -i echo {} 'file(s) extracted' && rm /var/www/bookstack/storage.tgz && exit"
 echo -e "\nRecreating $BOOKSTACK_APP_LABEL pod..."
 kubectl scale --replicas=0 deploy -l app=$BOOKSTACK_APP_LABEL --namespace=$WIKI_NAMSPACE
 kubectl scale --replicas=1 deploy -l app=$BOOKSTACK_APP_LABEL --namespace=$WIKI_NAMSPACE
