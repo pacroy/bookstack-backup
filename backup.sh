b90e2d5e88185a5a224c20f95eec88f22f696bd4
Par <pacroy@gmail.com>
2021-10-11 17:14:51 +0700

    Update new lines

--- backup.sh
+++ backup.sh
@@ -13,7 +13,6 @@ echo "KUBE_CONTEXT       : $KUBE_CONTEXT"
 echo "WIKI_NAMSPACE      : $WIKI_NAMSPACE"
 echo "MYSQL_APP_LABEL    : $MYSQL_APP_LABEL"
 echo "BOOKSTACK_APP_LABEL: $BOOKSTACK_APP_LABEL"
-echo
 
 if [ -z "$1" ] || [ $1 != '-y' ]; then
     read -p "Press [Enter] to backup from $KUBE_CONTEXT/$WIKI_NAMSPACE..."
@@ -24,7 +23,7 @@ MYSQL_PODS="$(kubectl get pod -o name -l app="$MYSQL_APP_LABEL" --context "$KUBE
 if [ -z "$MYSQL_PODS" ]; then echo "ERROR: Cannot find any $MYSQL_APP_LABEL pod" >&2 && exit 90; fi
 MYSQL_POD_NAME="$(echo ${MYSQL_PODS} | head -1 | grep -o '[^/]*$')"
 
-printf "\nCopying BookStack MySQL DB from $MYSQL_POD_NAME...\n"
+printf "Copying BookStack MySQL DB from $MYSQL_POD_NAME...\n"
 kubectl exec --context "$KUBE_CONTEXT" --namespace="$WIKI_NAMSPACE" --container="bookstack-mysql" "$MYSQL_POD_NAME" -- bash -c "MYSQL_PWD=secret mysqldump --all-databases" > ./backup/bookstack.sql
 
 # Backup Bookstack
@@ -32,8 +31,8 @@ BOOKSTACK_PODS="$(kubectl get pod -o name -l app="$BOOKSTACK_APP_LABEL" --contex
 if [ -z "$BOOKSTACK_PODS" ]; then echo "ERROR: Cannot find any $BOOKSTACK_APP_LABEL pod" >&2 && exit 90; fi
 BOOKSTACK_POD_NAME="$(echo ${BOOKSTACK_PODS} | head -1 | grep -o '[^/]*$')"
 
-printf "\nCopying BookStack Uploads from $BOOKSTACK_POD_NAME...\n"
+printf "Copying BookStack Uploads from $BOOKSTACK_POD_NAME...\n"
 kubectl exec --context "$KUBE_CONTEXT" --namespace="$WIKI_NAMSPACE" --container="bookstack" "$BOOKSTACK_POD_NAME" -- bash -c "cd /var/www/bookstack/public/uploads && tar -czf - *" > ./backup/uploads.tgz
 
-printf "\nCopying BookStack Storage from $BOOKSTACK_POD_NAME...\n"
+printf "Copying BookStack Storage from $BOOKSTACK_POD_NAME...\n"
 kubectl exec --context "$KUBE_CONTEXT" --namespace="$WIKI_NAMSPACE" --container="bookstack" "$BOOKSTACK_POD_NAME" -- bash -c "cd /var/www/bookstack/storage/ && tar -czf - uploads" > ./backup/storage.tgz
