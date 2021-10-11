4fad9658b3db0020bf85b7a7cd9a16f2cf35feaf
Par <pacroy@gmail.com>
2021-10-11 21:15:00 +0700

    Replace copy+extract with one command

--- restore.sh
+++ restore.sh
@@ -33,23 +33,21 @@ else
     kubectl exec --context="$KUBE_CONTEXT" --namespace="$WIKI_NAMSPACE" --container="bookstack-mysql" "$MYSQL_POD_NAME" -- bash -c "sed -i'.bak' -e 's/$HOST_FROM/$HOST_TO/g' /root/bookstack.sql"
 fi 
 printf "\nRestoring MySQL DB on $MYSQL_POD_NAME...\n"
-kubectl exec -it --context="$KUBE_CONTEXT" --namespace="$WIKI_NAMSPACE" --container="bookstack-mysql" "$MYSQL_POD_NAME" -- bash -c "echo 'FLUSH PRIVILEGES;' >> /root/bookstack.sql && MYSQL_PWD=secret mysql < /root/bookstack.sql && rm /root/bookstack.sql && exit"
+kubectl exec --context="$KUBE_CONTEXT" --namespace="$WIKI_NAMSPACE" --container="bookstack-mysql" "$MYSQL_POD_NAME" -- bash -c "echo 'FLUSH PRIVILEGES;' >> /root/bookstack.sql && MYSQL_PWD=secret mysql < /root/bookstack.sql && rm /root/bookstack.sql"
 
 # Restore Bookstack
 BOOKSTACK_PODS="$(kubectl get pod -o name -l app="$BOOKSTACK_APP_LABEL" --context "$KUBE_CONTEXT" --namespace="$WIKI_NAMSPACE")"
 if [ -z "$BOOKSTACK_PODS" ]; then echo "ERROR: Cannot find any $BOOKSTACK_APP_LABEL pod" >&2 && exit 90; fi
 BOOKSTACK_POD_NAME="$(echo "${BOOKSTACK_PODS}" | head -1 | grep -o '[^/]*$')"
 
-printf "\nCopying Bookstack Uploads Backup into $BOOKSTACK_POD_NAME...\n"
-kubectl cp --context $KUBE_CONTEXT --namespace=$WIKI_NAMSPACE ./backup/uploads.tgz -c bookstack $BOOKSTACK_POD_NAME:/var/www/bookstack/uploads.tgz
-printf "\nExtracting Bookstack Uploads Backup on $BOOKSTACK_POD_NAME...\n"
-kubectl exec -it --context $KUBE_CONTEXT --namespace=$WIKI_NAMSPACE -c bookstack $BOOKSTACK_POD_NAME -- bash -c "tar -xvzf /var/www/bookstack/uploads.tgz -C /var/www/bookstack/public/uploads | wc -l | xargs -i echo {} 'file(s) extracted' && rm /var/www/bookstack/uploads.tgz && exit"
-printf "\nCopying Bookstack Storage Backup into $BOOKSTACK_POD_NAME...\n"
-kubectl cp --context $KUBE_CONTEXT --namespace=$WIKI_NAMSPACE ./backup/storage.tgz -c bookstack $BOOKSTACK_POD_NAME:/var/www/bookstack/storage.tgz
-printf "\nExtracting Bookstack Storage Backup on $BOOKSTACK_POD_NAME...\n"
-kubectl exec -it --context $KUBE_CONTEXT --namespace=$WIKI_NAMSPACE -c bookstack $BOOKSTACK_POD_NAME -- bash -c "tar -xvzf /var/www/bookstack/storage.tgz -C /var/www/bookstack/storage | wc -l | xargs -i echo {} 'file(s) extracted' && rm /var/www/bookstack/storage.tgz && exit"
+printf "\nCopying Bookstack Uploads into $BOOKSTACK_POD_NAME...\n"
+cat ./backup/uploads.tgz | kubectl exec -i --context "$KUBE_CONTEXT" --namespace="$WIKI_NAMSPACE" --container="bookstack" "$MYSQL_POD_NAME" -- tar -xzf - -C /var/www/bookstack/public/uploads
+
+printf "\nCopying Bookstack Storage into $BOOKSTACK_POD_NAME...\n"
+cat ./backup/storage.tgz | kubectl exec -i --context "$KUBE_CONTEXT" --namespace="$WIKI_NAMSPACE" --container="bookstack" "$MYSQL_POD_NAME" -- tar -xzf - -C /var/www/bookstack/storage
+
 printf "\nRecreating $BOOKSTACK_APP_LABEL pod...\n"
-kubectl scale --replicas=0 deploy -l app=$BOOKSTACK_APP_LABEL --namespace=$WIKI_NAMSPACE
-kubectl scale --replicas=1 deploy -l app=$BOOKSTACK_APP_LABEL --namespace=$WIKI_NAMSPACE
+kubectl scale --replicas=0 deploy -l app="$BOOKSTACK_APP_LABEL" --namespace="$WIKI_NAMSPACE"
+kubectl scale --replicas=1 deploy -l app="$BOOKSTACK_APP_LABEL" --namespace="$WIKI_NAMSPACE"
 
 printf "\nNOTE: If Bookstack pod is error. Check logs and if it complains about table 'api_tokens', drop the table.\n"
