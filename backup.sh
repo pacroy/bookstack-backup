a3cd311da25afc8e9d833c27c4ecdcb770c38d66
Chairat Onyaem (Par) <pacroy@gmail.com>
2021-11-20 14:44:05 +0700

    Add linters and check md links workflow (#2)
    * Add linters and check md links workflow
    
    * Fix shellcheck linting issues
    
    * Adjust tar using -C argument

--- backup.sh
+++ backup.sh
@@ -17,8 +17,8 @@ echo "MYSQL_APP_LABEL    : $MYSQL_APP_LABEL"
 echo "BOOKSTACK_APP_LABEL: $BOOKSTACK_APP_LABEL"
 echo
 
-if [ -z "$1" ] || [ $1 != '-y' ]; then
-    read -p "Press [Enter] to backup from $KUBE_CONTEXT/$WIKI_NAMSPACE..."
+if [ -z "$1" ] || [ "$1" != '-y' ]; then
+    read -rp "Press [Enter] to backup from $KUBE_CONTEXT/$WIKI_NAMSPACE..."
     echo
 fi
 
@@ -27,7 +27,7 @@ MYSQL_PODS="$(kubectl get pod -o name -l app="$MYSQL_APP_LABEL" --context "$KUBE
 if [ -z "$MYSQL_PODS" ]; then echo "ERROR: Cannot find any $MYSQL_APP_LABEL pod" >&2 && exit 90; fi
 MYSQL_POD_NAME="$(echo "${MYSQL_PODS}" | head -1 | grep -o '[^/]*$')"
 
-printf "Copying BookStack MySQL DB from $MYSQL_POD_NAME...\n"
+printf "Copying BookStack MySQL DB from %s ...\n" "$MYSQL_POD_NAME"
 kubectl exec --context "$KUBE_CONTEXT" --namespace="$WIKI_NAMSPACE" --container="$MYSQL_CONTAINER" "$MYSQL_POD_NAME" -- bash -c "MYSQL_PWD=secret mysqldump --all-databases" > ./backup/bookstack.sql
 
 # Backup Bookstack
@@ -35,8 +35,8 @@ BOOKSTACK_PODS="$(kubectl get pod -o name -l app="$BOOKSTACK_APP_LABEL" --contex
 if [ -z "$BOOKSTACK_PODS" ]; then echo "ERROR: Cannot find any $BOOKSTACK_APP_LABEL pod" >&2 && exit 90; fi
 BOOKSTACK_POD_NAME="$(echo "${BOOKSTACK_PODS}" | head -1 | grep -o '[^/]*$')"
 
-printf "Copying BookStack Uploads from $BOOKSTACK_POD_NAME...\n"
-kubectl exec --context "$KUBE_CONTEXT" --namespace="$WIKI_NAMSPACE" --container="$BOOKSTACK_CONTAINER" "$BOOKSTACK_POD_NAME" -- bash -c "cd /var/www/bookstack/public/uploads && tar -czf - *" > ./backup/uploads.tgz
+printf "Copying BookStack Uploads from %s ...\n" "$BOOKSTACK_POD_NAME"
+kubectl exec --context "$KUBE_CONTEXT" --namespace="$WIKI_NAMSPACE" --container="$BOOKSTACK_CONTAINER" "$BOOKSTACK_POD_NAME" -- bash -c "tar -czf - -C /var/www/bookstack/public/uploads *" > ./backup/uploads.tgz
 
-printf "Copying BookStack Storage from $BOOKSTACK_POD_NAME...\n"
-kubectl exec --context "$KUBE_CONTEXT" --namespace="$WIKI_NAMSPACE" --container="$BOOKSTACK_CONTAINER" "$BOOKSTACK_POD_NAME" -- bash -c "cd /var/www/bookstack/storage/ && tar -czf - uploads" > ./backup/storage.tgz
+printf "Copying BookStack Storage from %s ...\n" "$BOOKSTACK_POD_NAME"
+kubectl exec --context "$KUBE_CONTEXT" --namespace="$WIKI_NAMSPACE" --container="$BOOKSTACK_CONTAINER" "$BOOKSTACK_POD_NAME" -- bash -c "tar -czf - -C /var/www/bookstack/storage uploads" > ./backup/storage.tgz
