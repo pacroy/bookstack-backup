736735b591cc4e8af6fb360b75d3fb477a844c89
Par <pacroy@gmail.com>
2021-11-20 14:35:41 +0700

    Fix shellcheck linting issues

--- restore.sh
+++ restore.sh
@@ -17,8 +17,8 @@ echo "MYSQL_APP_LABEL    : $MYSQL_APP_LABEL"
 echo "BOOKSTACK_APP_LABEL: $BOOKSTACK_APP_LABEL"
 echo
 
-if [ -z "$1" ] || [ $1 != '-y' ]; then
-    read -p "Press [Enter] to restore into $KUBE_CONTEXT/$WIKI_NAMSPACE..."
+if [ -z "$1" ] || [ "$1" != '-y' ]; then
+    read -rp "Press [Enter] to restore into $KUBE_CONTEXT/$WIKI_NAMSPACE..."
     echo
 fi
 
@@ -27,16 +27,16 @@ MYSQL_PODS="$(kubectl get pod -o name -l app="$MYSQL_APP_LABEL" --context "$KUBE
 if [ -z "$MYSQL_PODS" ]; then echo "ERROR: Cannot find any $MYSQL_APP_LABEL pod" >&2 && exit 90; fi
 MYSQL_POD_NAME="$(echo "${MYSQL_PODS}" | head -1 | grep -o '[^/]*$')"
 
-printf "Copying MySQL DB Backup into $MYSQL_POD_NAME...\n"
+printf "Copying MySQL DB Backup into %s ...\n" "$MYSQL_POD_NAME"
 tar -czf - ./backup/bookstack.sql | kubectl exec -i --context "$KUBE_CONTEXT" --namespace="$WIKI_NAMSPACE" --container="$MYSQL_CONTAINER" "$MYSQL_POD_NAME" -- tar -xzf - -C /root
 
 if { [ -z "$HOST_FROM" ] || [ -z "$HOST_TO" ]; }; then 
     printf "HOST_FROM and/or HOST_TO not specified. Skip updating hostname.\n"
 else
-    printf "Updating hostname from '$HOST_FROM' to '$HOST_TO'...\n"
+    printf "Updating hostname from '%s' to '%s' ...\n" "$HOST_FROM" "$HOST_TO"
     kubectl exec --context="$KUBE_CONTEXT" --namespace="$WIKI_NAMSPACE" --container="$MYSQL_CONTAINER" "$MYSQL_POD_NAME" -- bash -c "sed -i'.bak' -e 's/$HOST_FROM/$HOST_TO/g' /root/bookstack.sql"
 fi 
-printf "\nRestoring MySQL DB on $MYSQL_POD_NAME...\n"
+printf "\nRestoring MySQL DB on %s ...\n" "$MYSQL_POD_NAME"
 kubectl exec --context="$KUBE_CONTEXT" --namespace="$WIKI_NAMSPACE" --container="$MYSQL_CONTAINER" "$MYSQL_POD_NAME" -- bash -c "echo 'FLUSH PRIVILEGES;' >> /root/bookstack.sql && MYSQL_PWD=secret mysql < /root/bookstack.sql && rm /root/bookstack.sql"
 
 # Restore Bookstack
@@ -44,13 +44,13 @@ BOOKSTACK_PODS="$(kubectl get pod -o name -l app="$BOOKSTACK_APP_LABEL" --contex
 if [ -z "$BOOKSTACK_PODS" ]; then echo "ERROR: Cannot find any $BOOKSTACK_APP_LABEL pod" >&2 && exit 90; fi
 BOOKSTACK_POD_NAME="$(echo "${BOOKSTACK_PODS}" | head -1 | grep -o '[^/]*$')"
 
-printf "Copying Bookstack Uploads into $BOOKSTACK_POD_NAME...\n"
-cat ./backup/uploads.tgz | kubectl exec -i --context "$KUBE_CONTEXT" --namespace="$WIKI_NAMSPACE" --container="$BOOKSTACK_CONTAINER" "$BOOKSTACK_POD_NAME" -- tar -xzf - -C /var/www/bookstack/public/uploads
+printf "Copying Bookstack Uploads into %s ...\n" "$BOOKSTACK_POD_NAME"
+kubectl exec -i --context "$KUBE_CONTEXT" --namespace="$WIKI_NAMSPACE" --container="$BOOKSTACK_CONTAINER" "$BOOKSTACK_POD_NAME" -- tar -xzf - -C /var/www/bookstack/public/uploads < ./backup/uploads.tgz
 
-printf "Copying Bookstack Storage into $BOOKSTACK_POD_NAME...\n"
-cat ./backup/storage.tgz | kubectl exec -i --context "$KUBE_CONTEXT" --namespace="$WIKI_NAMSPACE" --container="$BOOKSTACK_CONTAINER" "$BOOKSTACK_POD_NAME" -- tar -xzf - -C /var/www/bookstack/storage
+printf "Copying Bookstack Storage into %s ...\n" "$BOOKSTACK_POD_NAME"
+kubectl exec -i --context "$KUBE_CONTEXT" --namespace="$WIKI_NAMSPACE" --container="$BOOKSTACK_CONTAINER" "$BOOKSTACK_POD_NAME" -- tar -xzf - -C /var/www/bookstack/storage < ./backup/storage.tgz
 
-printf "Recreating $BOOKSTACK_APP_LABEL pod...\n"
+printf "Recreating %s pod ...\n" "$BOOKSTACK_APP_LABEL"
 kubectl scale --replicas=0 deploy -l app="$BOOKSTACK_APP_LABEL" --namespace="$WIKI_NAMSPACE"
 kubectl scale --replicas=1 deploy -l app="$BOOKSTACK_APP_LABEL" --namespace="$WIKI_NAMSPACE"
 
