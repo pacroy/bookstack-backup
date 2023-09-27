96e1ce3872bfa84160c2298ad367975d4db5c5a1
Chairat Onyaem (Par) <pacroy@users.noreply.github.com>
2023-09-27 21:32:41 +0700

    Show no output when upload to storage (#5)

--- README.md
+++ README.md
@@ -1,52 +1,70 @@
-# Bookstack Backup Scripts
+# Bookstack Backup & Restore
 
-Shell scripts to back up and restore Bookstack + MySQL on a Kubernetes cluster.
+This repository contians bash scripts and GitHub Actions workflows to backup and restore Bookstack nad its MySQL deployed on a [Microk8s cluster](https://github.com/pacroy/microk8s-azure-vm) using [this Helm chart](https://github.com/pacroy/bookstack-helm).
 
-## Prerequisites
+## CLI Usage
 
-In order to use [backup](backup.sh) and [restore](restore.sh) script, you need:
+### CLI Usage - Prerequisites
 
-- `kubectl` with the context of the cluster to backup
+1. Make sure [kubectl](https://kubernetes.io/docs/tasks/tools/) is installed, the current context is configured, and it can connect to the cluster successfully.
+2. The following environment variables are set:
 
-## Backup
+    ```bash
+    export KUBE_CONTEXT="microk8s"
+    export WIKI_NAMESPACE="wiki"
+    export MYSQL_APP_LABEL="release-mysql"
+    export BOOKSTACK_APP_LABEL="release-bookstack"
+    ```
 
-Create and execute shell script file like this:
+### CLI Usage - Backup
 
-```sh
-#!/bin/bash
-set -e
+Execute the script.
 
-# Parameters
-export KUBE_CONTEXT=<Kubectl Context Name>
-export WIKI_NAMSPACE=<Kubernetes Namespace>
-export MYSQL_APP_LABEL=<App Label of MySQL>
-export BOOKSTACK_APP_LABEL=<App Label of Bookstack>
-
-# Execute
-bash -e <(curl -s https://raw.githubusercontent.com/pacroy/bookstack-backup/master/backup.sh)
+```bash
+source <(curl -s https://raw.githubusercontent.com/pacroy/bookstack-backup/main/backup.sh)
 ```
 
-## Restore
+### CLI Usage - Restore
 
-Create and execute shell script file like this:
-
-```sh
-#!/bin/bash
-set -e
-
-# Parameters
-export KUBE_CONTEXT=<Kubernetes Context Name>
-export WIKI_NAMSPACE=<Bookstack Namespace>
-export MYSQL_APP_LABEL=<App Label of MySQL>
-export BOOKSTACK_APP_LABEL=<App Label of Bookstack>
-
-# Execute
-bash -e <(curl -s https://raw.githubusercontent.com/pacroy/bookstack-backup/master/restore.sh)
-```
-
-You can additionally set the following variables if you copyback from one environment to another.
+If you copyback from one environment to another, you can additionally set the following variables to update all links.
 
 ```sh
-export HOST_FROM=<wiki.yourdomain.com>
-export HOST_TO=<wiki2.yourdomain.com>
+export HOST_FROM="wiki.yourdomain.com"
+export HOST_TO="wiki2.yourdomain.com"
 ```
+
+Execute the script.
+
+```bash
+bash -e <(curl -s https://raw.githubusercontent.com/pacroy/bookstack-backup/main/restore.sh)
+```
+
+## GitHub Actions Usage
+
+### GitHub Actions Usage - Prerequisites
+
+1. Create AzureAD application, if you don't already have one.
+2. Grant the application so it can access storage account.
+3. [Configure OIDC federated credential](https://learn.microsoft.com/en-us/azure/developer/github/connect-from-azure?tabs=azure-portal%2Clinux) in your application to allow GitHub Actions to acess your Azure environment.
+4. Fork or clone this repository into yours.
+5. Go to your repository settings and create a new environment `production`.
+6. Add the following environment secrets:
+
+| Name | Description |
+|---|---|
+| AZURE_CLIENT_ID | AzureAD application client ID |
+| AZURE_SUBSCRIPTION_ID | Azure subscription ID |
+| AZURE_TENANT_ID | Azure tenant ID|
+| BLOB_CONTAINER_NAME | Blob container name within Azure storage for storing backup files |
+| BOOKSTACK_APP_LABEL | Bookstack pod label e.g. `release-bookstack` |
+| KUBE_API_SERVER | Kubeconfig clusters.cluster.server |
+| KUBE_CA_BASE64 | Kubeconfig clusters.cluster.certificate-authority-data |
+| KUBE_CONTEXT | Kubeconfig contexts.context.name |
+| KUBE_USER_TOKEN | Kubeconfig users.user.token |
+| MYSQL_APP_LABEL | MySQL pod label e.g. `release-mysql` |
+| STORAGE_ACCOUNT_NAME | Azure storage account name for storing backup files |
+| WIKI_NAMESPACE | Kubernetes namespace containing bookstack release |
+
+### GitHub Actions Usage - Backup
+
+The `Backup` workflow is configured to run every Sunday's 0:00. You can manually run it at anytime you want.
--- restore.sh
+++ restore.sh
@@ -4,7 +4,7 @@ set -o pipefail
 
 # Check Parameters
 [ -z "$KUBE_CONTEXT" ] && echo "ERROR: Environment variable KUBE_CONTEXT is not set" && exit 1
-[ -z "$WIKI_NAMSPACE" ] && echo "ERROR: Environment variable WIKI_NAMSPACE is not set" && exit 1
+[ -z "$WIKI_NAMESPACE" ] && echo "ERROR: Environment variable WIKI_NAMESPACE is not set" && exit 1
 [ -z "$MYSQL_APP_LABEL" ] && echo "ERROR: Environment variable MYSQL_APP_LABEL is not set" && exit 1
 [ -z "$BOOKSTACK_APP_LABEL" ] && echo "ERROR: Environment variable MYSQL_APP_LABEL is not set" && exit 1
 MYSQL_CONTAINER="bookstack-mysql"
@@ -12,46 +12,46 @@ BOOKSTACK_CONTAINER="bookstack"
 
 # Print parameters
 echo "KUBE_CONTEXT       : $KUBE_CONTEXT"
-echo "WIKI_NAMSPACE      : $WIKI_NAMSPACE"
+echo "WIKI_NAMESPACE      : $WIKI_NAMESPACE"
 echo "MYSQL_APP_LABEL    : $MYSQL_APP_LABEL"
 echo "BOOKSTACK_APP_LABEL: $BOOKSTACK_APP_LABEL"
 echo
 
 if [ -z "$1" ] || [ "$1" != '-y' ]; then
-    read -rp "Press [Enter] to restore into $KUBE_CONTEXT/$WIKI_NAMSPACE..."
+    read -rp "Press [Enter] to restore into $KUBE_CONTEXT/$WIKI_NAMESPACE..."
     echo
 fi
 
 # Restore MySQL
-MYSQL_PODS="$(kubectl get pod -o name -l app="$MYSQL_APP_LABEL" --context "$KUBE_CONTEXT" --namespace="$WIKI_NAMSPACE")"
+MYSQL_PODS="$(kubectl get pod -o name -l app="$MYSQL_APP_LABEL" --context "$KUBE_CONTEXT" --namespace="$WIKI_NAMESPACE")"
 if [ -z "$MYSQL_PODS" ]; then echo "ERROR: Cannot find any $MYSQL_APP_LABEL pod" >&2 && exit 90; fi
 MYSQL_POD_NAME="$(echo "${MYSQL_PODS}" | head -1 | grep -o '[^/]*$')"
 
 printf "Copying MySQL DB Backup into %s ...\n" "$MYSQL_POD_NAME"
-tar -czf - -C ./backup bookstack.sql | kubectl exec -i --context "$KUBE_CONTEXT" --namespace="$WIKI_NAMSPACE" --container="$MYSQL_CONTAINER" "$MYSQL_POD_NAME" -- tar -xzf - -C /root
+tar -czf - -C ./backup bookstack.sql | kubectl exec -i --context "$KUBE_CONTEXT" --namespace="$WIKI_NAMESPACE" --container="$MYSQL_CONTAINER" "$MYSQL_POD_NAME" -- tar -xzf - -C /root
 
 if { [ -z "$HOST_FROM" ] || [ -z "$HOST_TO" ]; }; then 
     printf "HOST_FROM and/or HOST_TO not specified. Skip updating hostname.\n"
 else
     printf "Updating hostname from '%s' to '%s' ...\n" "$HOST_FROM" "$HOST_TO"
-    kubectl exec --context="$KUBE_CONTEXT" --namespace="$WIKI_NAMSPACE" --container="$MYSQL_CONTAINER" "$MYSQL_POD_NAME" -- bash -c "sed -i'.bak' -e 's/$HOST_FROM/$HOST_TO/g' /root/bookstack.sql"
+    kubectl exec --context="$KUBE_CONTEXT" --namespace="$WIKI_NAMESPACE" --container="$MYSQL_CONTAINER" "$MYSQL_POD_NAME" -- bash -c "sed -i'.bak' -e 's/$HOST_FROM/$HOST_TO/g' /root/bookstack.sql"
 fi 
 printf "\nRestoring MySQL DB on %s ...\n" "$MYSQL_POD_NAME"
-kubectl exec --context="$KUBE_CONTEXT" --namespace="$WIKI_NAMSPACE" --container="$MYSQL_CONTAINER" "$MYSQL_POD_NAME" -- bash -c "echo 'FLUSH PRIVILEGES;' >> /root/bookstack.sql && MYSQL_PWD=secret mysql < /root/bookstack.sql && rm /root/bookstack.sql"
+kubectl exec --context="$KUBE_CONTEXT" --namespace="$WIKI_NAMESPACE" --container="$MYSQL_CONTAINER" "$MYSQL_POD_NAME" -- bash -c "echo 'FLUSH PRIVILEGES;' >> /root/bookstack.sql && MYSQL_PWD=secret mysql < /root/bookstack.sql && rm /root/bookstack.sql"
 
 # Restore Bookstack
-BOOKSTACK_PODS="$(kubectl get pod -o name -l app="$BOOKSTACK_APP_LABEL" --context "$KUBE_CONTEXT" --namespace="$WIKI_NAMSPACE")"
+BOOKSTACK_PODS="$(kubectl get pod -o name -l app="$BOOKSTACK_APP_LABEL" --context "$KUBE_CONTEXT" --namespace="$WIKI_NAMESPACE")"
 if [ -z "$BOOKSTACK_PODS" ]; then echo "ERROR: Cannot find any $BOOKSTACK_APP_LABEL pod" >&2 && exit 90; fi
 BOOKSTACK_POD_NAME="$(echo "${BOOKSTACK_PODS}" | head -1 | grep -o '[^/]*$')"
 
 printf "Copying Bookstack Uploads into %s ...\n" "$BOOKSTACK_POD_NAME"
-kubectl exec -i --context "$KUBE_CONTEXT" --namespace="$WIKI_NAMSPACE" --container="$BOOKSTACK_CONTAINER" "$BOOKSTACK_POD_NAME" -- tar -xzf - -C /var/www/bookstack/public/uploads < ./backup/uploads.tgz
+kubectl exec -i --context "$KUBE_CONTEXT" --namespace="$WIKI_NAMESPACE" --container="$BOOKSTACK_CONTAINER" "$BOOKSTACK_POD_NAME" -- tar -xzf - -C /var/www/bookstack/public/uploads < ./backup/uploads.tgz
 
 printf "Copying Bookstack Storage into %s ...\n" "$BOOKSTACK_POD_NAME"
-kubectl exec -i --context "$KUBE_CONTEXT" --namespace="$WIKI_NAMSPACE" --container="$BOOKSTACK_CONTAINER" "$BOOKSTACK_POD_NAME" -- tar -xzf - -C /var/www/bookstack/storage < ./backup/storage.tgz
+kubectl exec -i --context "$KUBE_CONTEXT" --namespace="$WIKI_NAMESPACE" --container="$BOOKSTACK_CONTAINER" "$BOOKSTACK_POD_NAME" -- tar -xzf - -C /var/www/bookstack/storage < ./backup/storage.tgz
 
 printf "Recreating %s pod ...\n" "$BOOKSTACK_APP_LABEL"
-kubectl scale --replicas=0 deploy -l app="$BOOKSTACK_APP_LABEL" --namespace="$WIKI_NAMSPACE"
-kubectl scale --replicas=1 deploy -l app="$BOOKSTACK_APP_LABEL" --namespace="$WIKI_NAMSPACE"
+kubectl scale --replicas=0 deploy -l app="$BOOKSTACK_APP_LABEL" --namespace="$WIKI_NAMESPACE"
+kubectl scale --replicas=1 deploy -l app="$BOOKSTACK_APP_LABEL" --namespace="$WIKI_NAMESPACE"
 
 printf "\nNOTE: If Bookstack pod is error. Check logs and if it complains about table 'api_tokens', drop the table.\n"
