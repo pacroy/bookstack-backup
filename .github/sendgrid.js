120a5c1f5347592d866de0d052a3dd768cc3ca15
Chairat Onyaem (Par) <pacroy@users.noreply.github.com>
2023-10-01 06:56:31 +0000

    Add email workflow

---
+++ .github/sendgrid.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+const msg = {
+    to: process.env.SENDGRID_RECIPIENTS,
+    from: '41898282+github-actions[bot]@users.noreply.github.com',
+    subject: 'Hello world',
+    text: 'Hello plain world!',
+    html: '<p>Hello HTML world!</p>',
+};
+
+sgMail
+    .send(msg)
+    .then(() => console.log('Mail sent successfully'))
+    .catch(error => console.error(error.toString()));
