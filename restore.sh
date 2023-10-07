393a38e9b78196ec0d46f2b6d77c2f2bfffc8403
Chairat Onyaem (Par) <pacroy@users.noreply.github.com>
2023-10-07 14:28:54 +0000

    Add duration measurements

--- restore.sh
+++ restore.sh
@@ -27,28 +27,48 @@ MYSQL_PODS="$(kubectl get pod -o name -l app="$MYSQL_APP_LABEL" --context "$KUBE
 if [ -z "$MYSQL_PODS" ]; then echo "ERROR: Cannot find any $MYSQL_APP_LABEL pod" >&2 && exit 90; fi
 MYSQL_POD_NAME="$(echo "${MYSQL_PODS}" | head -1 | grep -o '[^/]*$')"
 
-printf "Copying MySQL DB Backup into %s ...\n" "$MYSQL_POD_NAME"
+printf "Copying MySQL DB Backup into %s ... " "$MYSQL_POD_NAME"
+START=$(date +%s.%N)
 tar -czf - -C ./backup bookstack.sql | kubectl exec -i --context "$KUBE_CONTEXT" --namespace="$WIKI_NAMESPACE" --container="$MYSQL_CONTAINER" "$MYSQL_POD_NAME" -- tar -xzf - -C /root
+END=$(date +%s.%N)
+DIFF=$(echo "$END - $START" | bc)
+printf "%s seconds\n" "$DIFF"
 
 if { [ -z "$HOST_FROM" ] || [ -z "$HOST_TO" ]; }; then 
     printf "HOST_FROM and/or HOST_TO not specified. Skip updating hostname.\n"
 else
-    printf "Updating hostname from '%s' to '%s' ...\n" "$HOST_FROM" "$HOST_TO"
+    printf "Updating hostname from '%s' to '%s' ... " "$HOST_FROM" "$HOST_TO"
+    START=$(date +%s.%N)
     kubectl exec --context="$KUBE_CONTEXT" --namespace="$WIKI_NAMESPACE" --container="$MYSQL_CONTAINER" "$MYSQL_POD_NAME" -- bash -c "sed -i'.bak' -e 's/$HOST_FROM/$HOST_TO/g' /root/bookstack.sql"
+    END=$(date +%s.%N)
+    DIFF=$(echo "$END - $START" | bc)
+    printf "%s seconds\n" "$DIFF"
 fi 
-printf "\nRestoring MySQL DB on %s ...\n" "$MYSQL_POD_NAME"
+printf "\nRestoring MySQL DB on %s ... " "$MYSQL_POD_NAME"
+START=$(date +%s.%N)
 kubectl exec --context="$KUBE_CONTEXT" --namespace="$WIKI_NAMESPACE" --container="$MYSQL_CONTAINER" "$MYSQL_POD_NAME" -- bash -c "echo 'FLUSH PRIVILEGES;' >> /root/bookstack.sql && MYSQL_PWD=secret mysql < /root/bookstack.sql && rm /root/bookstack.sql"
+END=$(date +%s.%N)
+DIFF=$(echo "$END - $START" | bc)
+printf "%s seconds\n" "$DIFF"
 
 # Restore Bookstack
 BOOKSTACK_PODS="$(kubectl get pod -o name -l app="$BOOKSTACK_APP_LABEL" --context "$KUBE_CONTEXT" --namespace="$WIKI_NAMESPACE")"
 if [ -z "$BOOKSTACK_PODS" ]; then echo "ERROR: Cannot find any $BOOKSTACK_APP_LABEL pod" >&2 && exit 90; fi
 BOOKSTACK_POD_NAME="$(echo "${BOOKSTACK_PODS}" | head -1 | grep -o '[^/]*$')"
 
-printf "Copying Bookstack Uploads into %s ...\n" "$BOOKSTACK_POD_NAME"
+printf "Copying Bookstack Uploads into %s ... " "$BOOKSTACK_POD_NAME"
+START=$(date +%s.%N)
 kubectl exec -i --context "$KUBE_CONTEXT" --namespace="$WIKI_NAMESPACE" --container="$BOOKSTACK_CONTAINER" "$BOOKSTACK_POD_NAME" -- tar -xzf - -C /var/www/bookstack/public/uploads < ./backup/uploads.tgz
+END=$(date +%s.%N)
+DIFF=$(echo "$END - $START" | bc)
+printf "%s seconds\n" "$DIFF"
 
-printf "Copying Bookstack Storage into %s ...\n" "$BOOKSTACK_POD_NAME"
+printf "Copying Bookstack Storage into %s ... " "$BOOKSTACK_POD_NAME"
+START=$(date +%s.%N)
 kubectl exec -i --context "$KUBE_CONTEXT" --namespace="$WIKI_NAMESPACE" --container="$BOOKSTACK_CONTAINER" "$BOOKSTACK_POD_NAME" -- tar -xzf - -C /var/www/bookstack/storage < ./backup/storage.tgz
+END=$(date +%s.%N)
+DIFF=$(echo "$END - $START" | bc)
+printf "%s seconds\n" "$DIFF"
 
 printf "Recreating %s pod ...\n" "$BOOKSTACK_APP_LABEL"
 kubectl scale --replicas=0 deploy -l app="$BOOKSTACK_APP_LABEL" --namespace="$WIKI_NAMESPACE"
