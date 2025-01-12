465f5442d10f2853c60b9d908d7bf2494bb3f8d7
Chairat Onyaem (Par) <pacroy@users.noreply.github.com>
2025-01-12 14:07:50 +0700

    Temp comment bookstack restore script

--- restore.sh
+++ restore.sh
@@ -67,31 +67,31 @@ kubectl exec --context="$KUBE_CONTEXT" --namespace="$WIKI_NAMESPACE" --container
 stop_clock "%s seconds\n"
 echo
 
-# Restore Bookstack
-BOOKSTACK_PODS="$(kubectl get pod -o name -l app="$BOOKSTACK_APP_LABEL" --context "$KUBE_CONTEXT" --namespace="$WIKI_NAMESPACE")"
-if [ -z "$BOOKSTACK_PODS" ]; then echo "ERROR: Cannot find any $BOOKSTACK_APP_LABEL pod" >&2 && exit 90; fi
-BOOKSTACK_POD_NAME="$(echo "${BOOKSTACK_PODS}" | head -1 | grep -o '[^/]*$')"
+# # Restore Bookstack
+# BOOKSTACK_PODS="$(kubectl get pod -o name -l app="$BOOKSTACK_APP_LABEL" --context "$KUBE_CONTEXT" --namespace="$WIKI_NAMESPACE")"
+# if [ -z "$BOOKSTACK_PODS" ]; then echo "ERROR: Cannot find any $BOOKSTACK_APP_LABEL pod" >&2 && exit 90; fi
+# BOOKSTACK_POD_NAME="$(echo "${BOOKSTACK_PODS}" | head -1 | grep -o '[^/]*$')"
 
-printf "Copying Bookstack Uploads into %s ... " "$BOOKSTACK_POD_NAME"
-start_clock
-{
-    kubectl exec --quiet --stdin --context "$KUBE_CONTEXT" --namespace="$WIKI_NAMESPACE" --container="$BOOKSTACK_CONTAINER" "$BOOKSTACK_POD_NAME" -- tar -xzf - -C /var/www/bookstack/public/uploads < ./backup/uploads.tgz
-} || return_code="$?"
-stop_clock "%s seconds\n"
-handle_returncode_2 "$return_code"
-echo
+# printf "Copying Bookstack Uploads into %s ... " "$BOOKSTACK_POD_NAME"
+# start_clock
+# {
+#     kubectl exec --quiet --stdin --context "$KUBE_CONTEXT" --namespace="$WIKI_NAMESPACE" --container="$BOOKSTACK_CONTAINER" "$BOOKSTACK_POD_NAME" -- tar -xzf - -C /var/www/bookstack/public/uploads < ./backup/uploads.tgz
+# } || return_code="$?"
+# stop_clock "%s seconds\n"
+# handle_returncode_2 "$return_code"
+# echo
 
-printf "Copying Bookstack Storage into %s ... " "$BOOKSTACK_POD_NAME"
-start_clock
-{
-    kubectl exec --quiet --stdin --context "$KUBE_CONTEXT" --namespace="$WIKI_NAMESPACE" --container="$BOOKSTACK_CONTAINER" "$BOOKSTACK_POD_NAME" -- tar -xzf - -C /var/www/bookstack/storage < ./backup/storage.tgz
-} || return_code="$?"
-stop_clock "%s seconds\n"
-handle_returncode_2 "$return_code"
-echo
+# printf "Copying Bookstack Storage into %s ... " "$BOOKSTACK_POD_NAME"
+# start_clock
+# {
+#     kubectl exec --quiet --stdin --context "$KUBE_CONTEXT" --namespace="$WIKI_NAMESPACE" --container="$BOOKSTACK_CONTAINER" "$BOOKSTACK_POD_NAME" -- tar -xzf - -C /var/www/bookstack/storage < ./backup/storage.tgz
+# } || return_code="$?"
+# stop_clock "%s seconds\n"
+# handle_returncode_2 "$return_code"
+# echo
 
-printf "Recreating %s pod ...\n" "$BOOKSTACK_APP_LABEL"
-kubectl scale --replicas=0 deploy -l app="$BOOKSTACK_APP_LABEL" --namespace="$WIKI_NAMESPACE"
-kubectl scale --replicas=1 deploy -l app="$BOOKSTACK_APP_LABEL" --namespace="$WIKI_NAMESPACE"
+# printf "Recreating %s pod ...\n" "$BOOKSTACK_APP_LABEL"
+# kubectl scale --replicas=0 deploy -l app="$BOOKSTACK_APP_LABEL" --namespace="$WIKI_NAMESPACE"
+# kubectl scale --replicas=1 deploy -l app="$BOOKSTACK_APP_LABEL" --namespace="$WIKI_NAMESPACE"
 
-printf "\nNOTE: If Bookstack pod is error. Check logs and if it complains about table 'api_tokens', drop the table.\n"
+# printf "\nNOTE: If Bookstack pod is error. Check logs and if it complains about table 'api_tokens', drop the table.\n"
